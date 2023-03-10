a468a6783396b824b2652fa7a6b7f259f1fc616a
Drake Wong <40375132+drakejwong@users.noreply.github.com>
2023-03-10 02:04:13 -0800

    https://github.com/shawwn/llama-dl/issues/1#issuecomment-1458870564

--- download.sh
+++ download.sh
@@ -1,9 +1,10 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the GNU General Public License version 3.
 
-PRESIGNED_URL=""             # replace with presigned url from email
-MODEL_SIZE="7B,13B,30B,65B"  # edit this list with the model sizes you wish to download
-TARGET_FOLDER=""             # where all files should end up
+PRESIGNED_URL="https://agi.gpt4.org/llama/LLaMA/*"
+
+MODEL_SIZE="7B,13B"                # edit this list with the model sizes you wish to download
+TARGET_FOLDER="$HOME/llama-model/" # where all files should end up
 
 declare -A N_SHARD_DICT
 
@@ -18,16 +19,14 @@ wget ${PRESIGNED_URL/'*'/"tokenizer_checklist.chk"} -O ${TARGET_FOLDER}"/tokeniz
 
 (cd ${TARGET_FOLDER} && md5sum -c tokenizer_checklist.chk)
 
-for i in ${MODEL_SIZE//,/ }
-do
-    echo "Downloading ${i}"
-    mkdir -p ${TARGET_FOLDER}"/${i}"
-    for s in $(seq -f "0%g" 0 ${N_SHARD_DICT[$i]})
-    do
-        wget ${PRESIGNED_URL/'*'/"${i}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${i}/consolidated.${s}.pth"
-    done
-    wget ${PRESIGNED_URL/'*'/"${i}/params.json"} -O ${TARGET_FOLDER}"/${i}/params.json"
-    wget ${PRESIGNED_URL/'*'/"${i}/checklist.chk"} -O ${TARGET_FOLDER}"/${i}/checklist.chk"
-    echo "Checking checksums"
-    (cd ${TARGET_FOLDER}"/${i}" && md5sum -c checklist.chk)
+for i in ${MODEL_SIZE//,/ }; do
+	echo "Downloading ${i}"
+	mkdir -p ${TARGET_FOLDER}"/${i}"
+	for s in $(seq -f "0%g" 0 ${N_SHARD_DICT[$i]}); do
+		wget ${PRESIGNED_URL/'*'/"${i}/consolidated.${s}.pth"} -O ${TARGET_FOLDER}"/${i}/consolidated.${s}.pth"
+	done
+	wget ${PRESIGNED_URL/'*'/"${i}/params.json"} -O ${TARGET_FOLDER}"/${i}/params.json"
+	wget ${PRESIGNED_URL/'*'/"${i}/checklist.chk"} -O ${TARGET_FOLDER}"/${i}/checklist.chk"
+	echo "Checking checksums"
+	(cd ${TARGET_FOLDER}"/${i}" && md5sum -c checklist.chk)
 done
